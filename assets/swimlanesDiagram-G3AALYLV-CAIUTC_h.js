490a4c6c709d3d93436893ce220fe6849363dee8
Fogjw <Fogjw@users.noreply.github.com>
2026-09-06 14:11:57 +0000

    deploy: 5563d0230fda5eb539baeaa828d14e79748054ce

---
+++ assets/swimlanesDiagram-G3AALYLV-CAIUTC_h.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-DF_12dSv.js";import{_ as a}from"./mermaid.core-RrID8ONF.js";import"./chunk-5VM5RSS4-44GX47AK.js";import"./chunk-XXDRQBXY-RcIDDrHZ.js";import"./chunk-VR4S4FIN-nTFEJWJC.js";import"./chunk-32BRIVSS-CNJZmKqK.js";import"./index-DTgAoWs3.js";import"./channel-BN3Ug4Bo.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
